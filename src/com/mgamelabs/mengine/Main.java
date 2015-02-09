dd0027c07977234184c63cd0552e952d25b934c1
mtronicsDev <maxi.schmeller@live.de>
2015-02-09 21:25:34 +0100

    Reformatted main class.

--- src/com/mgamelabs/mengine/Main.java
+++ src/com/mgamelabs/mengine/Main.java
@@ -10,12 +10,7 @@ import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
         Preferences.setProperties(Resources.getProperties("data/mengine.properties"));
-
         Window w = new Window("FirstEverMEngine2.0Window", 1080, 720);
 
         while (glfwWindowShouldClose(w.getId()) == GL11.GL_FALSE)
